6d1d4bfe665fcd2452016fde3b29415c5a4ffad7
Hossein-Gholami <gholami.hosseinreza@gmail.com>
2023-10-27 15:48:49 +0330

    update api

--- src/api.py
+++ src/api.py
@@ -6,9 +6,15 @@ from src.routes import router as news_router
 
 app = FastAPI()
 
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
