1855d876a6ef01b524f05815f73de970dd95993a
Hossein-Gholami <gholami.hosseinreza@gmail.com>
2023-10-27 14:36:53 +0330

    update api

--- src/api.py
+++ src/api.py
@@ -6,15 +6,15 @@ from src.routes import router as news_router
 
 app = FastAPI()
 
-app.add_event_handler("startup", utils.create_start_app_handler(app))
-app.add_event_handler("shutdown", utils.create_stop_app_handler(app))
-
-app.include_router(news_router, tags=["news"], prefix="/news")
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+app.add_event_handler("startup", utils.create_start_app_handler(app))
+app.add_event_handler("shutdown", utils.create_stop_app_handler(app))
+
+app.include_router(news_router, tags=["news"], prefix="/news")
